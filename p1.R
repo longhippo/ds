e5739d574aeb6551eae0c2cb099115d50b55aa57
longhippo <122474617+longhippo@users.noreply.github.com>
2024-01-24 20:27:50 +0530

    Update p1.R

--- p1.R
+++ p1.R
@@ -1,24 +1,20 @@
-# practical 1 : Create one-dimensional data using series and perform various operations on it 
 library("methods")
 data <- c(1,2,3,4,5,6)
 
 cat("Data series",data, "\n")
 
-# mathematical operations
 cat("Sum: ", sum(data), "\n")
 cat("Product : ", prod(data), "\n")
 cat("Square Root : ", sqrt(data), "\n")
 cat("Exponential : ", exp(data), "\n")
 cat("Logarithm : ", log(data), "\n")
 
-# Statistical operations
 cat("Minimum : ", min(data), "\n")
 cat("Maximum : ", max(data), "\n")
 cat("Mean : ", mean(data), "\n")
 cat("Median : ", median(data), "\n")
 cat("Standard Deviation : ", sd(data), "\n")
 
-# Array manipulation
 cat("Sorted array : ", sort(data), "\n")
 cat("Reversed array : ", rev(data), "\n")
 cat("Unique value : ", unique(data), "\n")
